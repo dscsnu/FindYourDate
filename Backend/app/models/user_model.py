1b06fa5a7b1200e0c97203421aba3b75e9871399
rohitjg13 <jgrohit.exe@gmail.com>
2025-10-22 23:08:20 +0530

    added phone to user model

--- Backend/app/models/user_model.py
+++ Backend/app/models/user_model.py
@@ -9,6 +9,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String, unique=True, index=True, nullable=False)
     gender = Column(String, nullable=False)
     orientation = Column(String, nullable=False)
     accept_non_straight = Column(Boolean, default=True)
