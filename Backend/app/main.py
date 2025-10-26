90786fc310c7958b484d6a9d3d0152e312837c3f
rohitjg13 <jgrohit.exe@gmail.com>
2025-10-27 01:54:39 +0530

    CORS for frontend

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -23,11 +23,12 @@ app = FastAPI(title="FindYourDate API", version="1.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://Localhost:5173",  # Handle case variation
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
+        # "http://localhost:5173",
+        # "http://localhost:5174",
+        # "http://Localhost:5173",  # Handle case variation
+        # "http://127.0.0.1:5173",
+        # "http://127.0.0.1:5174",
+        "https://findyourdate.snioe.dev"
     ],
     allow_credentials=True,
     allow_methods=["*"],
