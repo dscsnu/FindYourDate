b9bab518fdaf2fedd56cdb277d5490530e603c4a
rohitjg13 <jgrohit.exe@gmail.com>
2025-10-26 10:51:18 +0530

    Added auth route in main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import users, chat
+from app.api import users, chat, auth
 from app.db.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
 
